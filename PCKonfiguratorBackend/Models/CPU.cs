ca060d799a963dafd2189e55268495111660f916
Sandro-B <Sandrobajic@outlook.com>
2025-02-13 11:49:52 +0100

    init-db

---
+++ PCKonfiguratorBackend/Models/CPU.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PCKonfiguratorBackend.Models
+{
+    public class CPU
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public decimal Price { get; set; }
+        public int CoreCount { get; set; }
+        public double BaseClock { get; set; }
+        public double BoostClock { get; set; }
+        public string Socket { get; set; }
+    }
+}
