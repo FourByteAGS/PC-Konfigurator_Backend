4ca4f0160d19c8a12ca0036570adac8c00b532da
Nils Zentgraf <nils.zentgraf@aol.com>
2025-02-15 17:55:49 +0100

    add new api interface to filter towers

--- PCKonfiguratorBackend/Controllers/TowerController.cs
+++ PCKonfiguratorBackend/Controllers/TowerController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PCKonfiguratorBackend.Interface;
+using PCKonfiguratorBackend.Models;
 
 namespace PCKonfiguratorBackend.Controllers
 {
@@ -22,6 +24,25 @@ namespace PCKonfiguratorBackend.Controllers
         public IActionResult GetAll(Guid token)
         {           
             return Ok(_db.Towers.Include(i => i.towerCompatibility).Include(i => i.dimensions).ToJson());
+        }
+
+        [HttpGet("GetTowerByTowerType")]
+        public IActionResult GetTowerByTowerType(Guid token, [FromQuery] TowerType towerType)
+        {
+            return Ok(_db.Towers.Include(i => i.towerCompatibility).Include(i => i.dimensions).Where(i => i.towerType.Value == towerType).ToJson());
+        }
+        
+        public IActionResult SetTowerAsSelected(Guid token, Guid towerId)
+        {
+            // if (!_authService.ValidateToken(token))
+            // {
+            //     return Unauthorized();
+            // }
+
+            var tower = _db.Towers.FirstOrDefault(i => i.id == towerId);
+            //save tower in SelectedProducts but we need a singelton for that ????
+            
+            return Ok();
         }
     }
 }
