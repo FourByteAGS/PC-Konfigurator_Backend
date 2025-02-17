9b5ba1daa04728b69e6490ae6ceb0f6d4532ce91
Nils Zentgraf <nils.zentgraf@aol.com>
2025-02-17 14:34:42 +0100

    fix again

--- PCKonfiguratorBackend/Controllers/AuthController.cs
+++ PCKonfiguratorBackend/Controllers/AuthController.cs
@@ -9,16 +9,24 @@ namespace PCKonfiguratorBackend.Controllers
     public class AuthController : ControllerBase
     {
         public readonly IAuthRepository authRepository;
-        public readonly List<ProductCollection> _productCollection;
+        public readonly List<ProductCollection> _productCollections;
 
-        public AuthController(IAuthRepository authRepository,List<ProductCollection> productCollection)
+        public AuthController(IAuthRepository authRepository,List<ProductCollection> productCollections)
         {
             this.authRepository = authRepository;
-            _productCollection = productCollection;
+            _productCollections = productCollections;
         }
 
         [HttpGet("gettoken")]
-        public IActionResult GetToken() => Ok(authRepository.GetNewToken());
+        public IActionResult GetToken()
+        {
+            var token = authRepository.GetNewToken();
+            
+            if(!_productCollections.Any()|| _productCollections.Where(x => x.token == token).Any())
+                _productCollections.Add(new ProductCollection(token));
+            
+            return Ok(token);
+        }
 
         [HttpGet("validatetoken")]
         public IActionResult ValidateToken(Guid token)
